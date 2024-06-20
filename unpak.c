130986bd1902e7573195e3261297a268a24123b7
Sergey Veneckiy <Sergey_Veneckiy@epam.com>
2024-06-20 18:07:51 +0400

    refactor: Refactor unpack.c

--- unpak.c
+++ unpak.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <zlib.h>
 #include <archive.h>
 #include <archive_entry.h>
 
 #define BUFFER_SIZE 8192
 
-int unpack_targz(const char* filepath) {
+int unpack_archive(const char* filepath, int (*unpack_func)(struct archive*)) {
     struct archive* a = archive_read_new();
-    archive_read_support_filter_all(a);
-    archive_read_support_format_all(a);
+    unpack_func(a);
 
     if (archive_read_open_filename(a, filepath, BUFFER_SIZE) != ARCHIVE_OK) {
         fprintf(stderr, "Error: Failed to open %s\n", filepath);
@@ -34,29 +32,14 @@ int unpack_targz(const char* filepath) {
     return 0;
 }
 
-int unpack_zip(const char* filepath) {
-    struct archive* a = archive_read_new();
+int unpack_targz(struct archive* a) {
+    archive_read_support_filter_all(a);
+    archive_read_support_format_all(a);
+    return 0;
+}
+
+int unpack_zip(struct archive* a) {
     archive_read_support_format_zip(a);
-
-    if (archive_read_open_filename(a, filepath, BUFFER_SIZE) != ARCHIVE_OK) {
-        fprintf(stderr, "Error: Failed to open %s\n", filepath);
-        archive_read_free(a);
-        return 1;
-    }
-
-    struct archive_entry* entry;
-    while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
-        const char* entry_path = archive_entry_pathname(entry);
-        archive_entry_set_pathname(entry, entry_path);
-
-        if (archive_read_extract(a, entry, 0) != ARCHIVE_OK) {
-            fprintf(stderr, "Error: Failed to extract %s\n", entry_path);
-            archive_read_free(a);
-            return 1;
-        }
-    }
-
-    archive_read_free(a);
     return 0;
 }
 
@@ -67,7 +50,7 @@ int main(int argc, char* argv[]) {
     }
 
     const char* filepath = argv[1];
-    char* ext = strrchr(filepath, '.');
+    const char* ext = strrchr(filepath, '.');
 
     if (ext == NULL) {
         fprintf(stderr, "Error: Invalid file extension\n");
@@ -75,26 +58,14 @@ int main(int argc, char* argv[]) {
     }
 
     int result;
-    switch (ext[1]) {
-        case 'g':
-            if (strcmp(ext, ".gz") == 0) {
-                result = unpack_targz(filepath);
-            } else {
-                fprintf(stderr, "Error: Unsupported file extension\n");
-                return 1;
-            }
-            break;
-        case 'z':
-            if (strcmp(ext, ".zip") == 0) {
-                result = unpack_zip(filepath);
-            } else {
-                fprintf(stderr, "Error: Unsupported file extension\n");
-                return 1;
-            }
-            break;
-        default:
-            fprintf(stderr, "Error: Unsupported file extension\n");
-            return 1;
+
+    if (strcmp(ext, ".gz") == 0 || strcmp(ext, ".tar.gz") == 0 || strcmp(ext, ".tgz") == 0) {
+        result = unpack_archive(filepath, unpack_targz);
+    } else if (strcmp(ext, ".zip") == 0) {
+        result = unpack_archive(filepath, unpack_zip);
+    } else {
+        fprintf(stderr, "Error: Unsupported file extension\n");
+        return 1;
     }
 
     return result;
